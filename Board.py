9ef2d33b04e1fbeb460083f4361c8ae4dff0f658
Gabriel <gablandry31@gmail.com>
2022-08-11 22:23:03 -0400

    Save from 2022-08-11

--- Board.py
+++ Board.py
@@ -5,6 +5,10 @@ import Chess
 from pieces.Pieces import Pieces
 
 
+def convert_coordinates_to_cases(pos):
+    return [pos[0] // Chess.CUBE_SIZE, pos[1] // Chess.CUBE_SIZE] # Todo to check
+
+
 class Board:
     def __init__(self, pygame):
         self.pygame = pygame
@@ -57,7 +61,6 @@ class Board:
 
                 if self.possible_moves:
                     for i in self.possible_moves:
-                        print("number: ", i)
                         possible_move = self.pygame.draw.rect(screen, "BLUE",
                                                               Rect(i[0] * Chess.CUBE_SIZE, i[1] * Chess.CUBE_SIZE,
                                                                    Chess.CUBE_SIZE, Chess.CUBE_SIZE))
@@ -67,13 +70,14 @@ class Board:
             b = b + 1
 
     def click_event(self, pos):
-        case = [pos[0 - 1] // Chess.CUBE_SIZE, pos[1 - 1] // Chess.CUBE_SIZE]
+        case = convert_coordinates_to_cases(pos)
+        print(case)
 
         piece_name = self.board[case[0]][case[1]]
 
         if piece_name:  # check if it's not empty
             self.selected = case
-            self.possible_moves = Pieces(piece_name, pos, self.board).get_possible_movement()
+            self.possible_moves = Pieces(piece_name, convert_coordinates_to_cases(pos), self.board).get_possible_movement()
             self.draw_actual_board()
         else:
             if self.selected:
@@ -82,5 +86,3 @@ class Board:
             self.selected = ""
             self.possible_moves = []
             self.draw_actual_board()
-
-        print(case[0], case[1])
--- pieces/Pieces.py
+++ pieces/Pieces.py
@@ -1,3 +1,6 @@
+import Board
+
+
 class Pieces:
     def __init__(self, name, pos, board):
         self.name = name
@@ -5,9 +8,6 @@ class Pieces:
         self.board = board
 
     def get_type(self):
-        print("here")
-        print(self.name)
-        print("here")
         return self.name.split("_")[1]
 
     def get_team(self):
@@ -18,10 +18,7 @@ class Pieces:
         match self.get_type():
             case "pawn":
                 if self.get_team() == "black":
-                    print("first", self.pos[0])
-                    print("second", self.pos[1]+1)
+                    possible_positions.append(self.pos)
 
-                    possible_positions.__add__([self.pos[0], self.pos[1]+1])
-
-        print(possible_positions)
+        print("possible positions:", possible_positions)
         return possible_positions
